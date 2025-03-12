02bd2ed0d94f3fb6f022b57e1ab37e044495a0fe
talentlqs <balzeren@gmail.com>
2025-03-11 21:49:02 -0700

    Added argument for book path

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from stats import get_book_text
 from stats import count_words
 from stats import char_dict
 from stats import print_report
+import sys
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]#"books/frankenstein.txt"
     frankenstein = get_book_text(path)
     words_in_book = count_words(frankenstein)
     dict = char_dict(frankenstein)
